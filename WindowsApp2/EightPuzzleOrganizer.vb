50ac2793beaf3ba66250e1b36440c16caf326783
Dennis Gahm <dennisgahm@hotmail.com>
2024-01-25 23:30:19 +1000

    test

---
+++ WindowsApp2/EightPuzzleOrganizer.vb
@@ -0,0 +1,10 @@
+﻿Public Class EightPuzzleOrganizer
+    Dim list As List(Of EightPuzzle) = New List(Of EightPuzzle)
+    Public Sub New()
+
+    End Sub
+
+    Public Sub Add(ByVal puzzle As EightPuzzle)
+
+    End Sub
+End Class
